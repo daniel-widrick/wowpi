c7e4046e9e743bee6c11f138e0e6df6b4854d8d8
root <=daniel@widrick.net>
2019-05-31 13:57:42 -0400

    Redo fix of crappy wordpress get_option and update_option functions. They Suck. Don't use them as much as they were.

--- includes/wowpi_item_data_processing.php
+++ includes/wowpi_item_data_processing.php
@@ -1,7 +1,7 @@
 <?php
 function wowpi_getItem($itemId)
 {
-    $returned = get_option('wowpi_items_'.$itemId);
+    $returned = wowpi_widrick_get_option('wowpi_items_'.$itemId);
     if(($returned==false) || (isset($returned['last_update']) && (intval($returned['last_update'])+(62*24*60*60))<current_time('timestamp'))) {
         $serviceUrl = 'https://' . wowpi_getRegion() . '.api.blizzard.net/wow/item/'.$itemId.'?locale=' . wowpi_getLocale() . '&access_token=' . wowpi_getToken();
         $curl_response = wowpi_retrieve_data($serviceUrl);
@@ -23,8 +23,8 @@ function wowpi_getItem($itemId)
         if (isset($returned['data']) && !empty($returned['data'])) {
             $returned['last_update'] = current_time('timestamp');
         }
-        update_option('wowpi_items_'.$itemId, $returned);
+        wowpi_widrick_update_option('wowpi_items_'.$itemId, $returned);
     }
-    $returned = get_option('wowpi_items_'.$itemId);
+    $returned = wowpi_widrick_get_option('wowpi_items_'.$itemId);
     return $returned['data'];
 }
